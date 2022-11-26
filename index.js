76cd4e0f363b568c3e94cdbf0ecc7a8a9d879b6f
ShourovHasan <shourovhasan@gmail.com>
2022-11-26 13:05:50 +0600

    delete any product by seller addeed

--- index.js
+++ index.js
@@ -169,6 +169,13 @@ const run = async () => {
             res.send(result);
         });
 
+        app.delete('/products/:id', verifyJWT, verifySeller, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(filter);
+            res.send(result);
+        })
+
     }
     finally {
         
